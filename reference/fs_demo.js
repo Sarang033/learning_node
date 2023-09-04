b522afcca6669deef7da6445404acaed07ab4a3c
Sarang Sharma <sarangchamp2--4>
2023-09-04 20:08:52 +0530

    fs and path module

---
+++ reference/fs_demo.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path= require('path')
+
+// Create folder on our system
+
+// fs.mkdir(path.join(__dirname, '/About'),{}, err =>{
+//     if(err) throw err;
+//     console.log('Folder created.....')
+// })
+
+// Create and write into a file
+
+fs.writeFile(path.join(__dirname,'/About','about.txt'),'hi my name is Sarang Sharma', err=>{
+    if(err) throw err;
+    console.log("File created and written to");
+})
