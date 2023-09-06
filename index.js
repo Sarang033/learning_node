754473d8cbac2749685a44b9eee6fc06562d4982
Sarang Sharma <sarangchamp2--4>
2023-09-06 22:31:00 +0530

    added http module

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-const Person =require('./person')
+const http = require('http')
+const path = require('path')
+const fs= require('fs')
 
-const person1 = new Person('Sarang Sharma', 20)
+const server = http.createServer((req,res) => {
+    // if(req.url === '/'){
+    //     fs.readFile(path.join(__dirname, 'public','index.html'), (err,content) =>{
+    //         if(err) throw err;
+    //         res.writeHead(200, {'Content-Type':'text/html'})
+    //         res.end(content)
+    //     })
+    // }    
+   
+    // if(req.url ==='/about'){
+    //     fs.readFile(path.join(__dirname,'public','about.html'),(err,content) =>{
+    //         if(err) throw err;
+    //         res.writeHead(200,{'Content-Type':'text/html'})
+    //         res.end(content)
+    //     })
+    // }
+    // if(req.url ==='/api/users'){
+    //     const users= [
+    //         {name:'Sarang',age:20},
+    //         {name:'Rohan',age:24}
+    //     ]
+    //     res.writeHead(200,{'Content-Type':'application/json'})
+    //     res.end(JSON.stringify(users))
+    // }
 
-person1.greeting();
+    //BUILD FILE PATH
+    let filePath=path.join(__dirname,'public',req.url ==='/'?'index.html':req.url)
+    console.log(filePath)
+    res.end()
+})
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
